233ad27762b98b4d1409c1040eea99bfd2276882
Vlad <v.rozanoff@gmail.com>
2020-03-29 23:51:26 +0300

    Implement delete for national park

--- NationalParkAPI/NationalParkAPI/Controllers/NationalParksController.cs
+++ NationalParkAPI/NationalParkAPI/Controllers/NationalParksController.cs
@@ -97,5 +97,25 @@ namespace NationalParkAPI.Controllers
             return NoContent();
 
         }
+
+        [HttpDelete("{nationalParkId:int}", Name = "DeleteNationalPark")]
+        public IActionResult DeleteNationalPark(int nationalParkId)
+        {
+            if (!_npRepo.NationalParkExists(nationalParkId))
+            {
+                return NotFound();
+            }
+
+            var nationalParkObj = _npRepo.GetNationalPark(nationalParkId);
+
+            if (!_npRepo.DeleteNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when updating the record {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+
+        }
     }
 }
