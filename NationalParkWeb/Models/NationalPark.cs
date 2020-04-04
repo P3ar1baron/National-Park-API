5b17d7e4179c1e7fd1c0c7f30b0c4037f163bd0a
Vlad <v.rozanoff@gmail.com>
2020-04-04 14:56:41 +0300

    Fix NationalPark model edit index and upsert view

--- NationalParkWeb/Models/NationalPark.cs
+++ NationalParkWeb/Models/NationalPark.cs
@@ -14,6 +14,8 @@ namespace NationalParkWeb.Models
         [Required]
         public string State { get; set; }
 
+        public byte[] Picture { get; set; }
+
         public DateTime Created { get; set; }
 
         public DateTime Established { get; set; }
