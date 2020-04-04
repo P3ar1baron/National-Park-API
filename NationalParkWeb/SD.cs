40ecbdc344654c54fdf2dd9be1f4ecfbb45eb7d8
Vlad <v.rozanoff@gmail.com>
2020-04-04 13:36:45 +0300

    Fix GetAsync method of repository in web project add html for upsert view add backslash in api path

--- NationalParkWeb/SD.cs
+++ NationalParkWeb/SD.cs
@@ -8,7 +8,7 @@ namespace NationalParkWeb
     public static class SD
     {
         public static string APIBaseUrl = "https://localhost:44341/";
-        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks";
-        public static string TrailAPIPath = APIBaseUrl + "api/v1/trails";
+        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks/";
+        public static string TrailAPIPath = APIBaseUrl + "api/v1/trails/";
     }
 }
