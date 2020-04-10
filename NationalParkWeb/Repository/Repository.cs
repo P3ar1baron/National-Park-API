c974338ae3ac63689889ca83ab2895e188194d7c
Vlad <v.rozanoff@gmail.com>
2020-04-11 01:22:51 +0300

    Add token in repository

--- NationalParkWeb/Repository/Repository.cs
+++ NationalParkWeb/Repository/Repository.cs
@@ -1,9 +1,9 @@
 ﻿using NationalParkWeb.Repository.IRepository;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,7 +17,7 @@ namespace NationalParkWeb.Repository
         {
             _clientFactory = clientFactory;
         }
-        public async Task<bool> CreateAsync(string url, T objToCreate)
+        public async Task<bool> CreateAsync(string url, T objToCreate, string token="")
         {
             var request = new HttpRequestMessage(HttpMethod.Post, url);
             if (objToCreate != null)
@@ -31,9 +31,13 @@ namespace NationalParkWeb.Repository
             }
 
             var client = _clientFactory.CreateClient();
+            if (token.Length != 0)
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
             HttpResponseMessage response = await client.SendAsync(request);
 
-            if (response.StatusCode == System.Net.HttpStatusCode.Created)
+            if (response.StatusCode == HttpStatusCode.Created)
             {
                 return true;
             }
@@ -43,11 +47,15 @@ namespace NationalParkWeb.Repository
             }
         }
 
-        public async Task<bool> DeleteAsync(string url, int Id)
+        public async Task<bool> DeleteAsync(string url, int Id, string token="")
         {
             var request = new HttpRequestMessage(HttpMethod.Delete, url+Id);
 
             var client = _clientFactory.CreateClient();
+            if (token.Length != 0)
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
             HttpResponseMessage response = await client.SendAsync(request);
 
             if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
@@ -57,11 +65,15 @@ namespace NationalParkWeb.Repository
             return false;
         }
 
-        public async Task<IEnumerable<T>> GetAllAsync(string url)
+        public async Task<IEnumerable<T>> GetAllAsync(string url, string token="")
         {
             var request = new HttpRequestMessage(HttpMethod.Get, url );
 
             var client = _clientFactory.CreateClient();
+            if (token.Length != 0)
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
             HttpResponseMessage response = await client.SendAsync(request);
 
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
@@ -73,11 +85,15 @@ namespace NationalParkWeb.Repository
             return null;
         }
 
-        public async Task<T> GetAsync(string url, int Id)
+        public async Task<T> GetAsync(string url, int Id, string token="")
         {
             var request = new HttpRequestMessage(HttpMethod.Get, url+Id);
 
             var client = _clientFactory.CreateClient();
+            if (token.Length != 0)
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
             HttpResponseMessage response = await client.SendAsync(request);
 
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
@@ -89,7 +105,7 @@ namespace NationalParkWeb.Repository
             return null;
         }
 
-        public async Task<bool> UpdateAsync(string url, T objToUpdate)
+        public async Task<bool> UpdateAsync(string url, T objToUpdate, string token="")
         {
             var request = new HttpRequestMessage(HttpMethod.Patch, url);
             if (objToUpdate != null)
@@ -103,6 +119,10 @@ namespace NationalParkWeb.Repository
             }
 
             var client = _clientFactory.CreateClient();
+            if (token.Length != 0)
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
             HttpResponseMessage response = await client.SendAsync(request);
 
             if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
